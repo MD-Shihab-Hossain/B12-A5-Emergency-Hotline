90a00303600e12c43163435f375e43c1fe80af5a
Shihab Hossain <shihabhossain6808@gmail.com>
2025-08-29 11:44:31 +0600

    Add Heart count functionality

--- index.js
+++ index.js
@@ -16,3 +16,27 @@ document.getElementById("menu-icon").addEventListener("click", function () {
   mobileMenu.classList.remove("hidden");
   mobileMenu.classList.add("flex");
 });
+//* function to get inner text
+function getInnerText(id) {
+  return document.getElementById(id).innerText;
+}
+//* function for add event listener for increasing heart count
+function addEventListenerToIncreaseHeart(id) {
+  document.getElementById(id).addEventListener("click", function () {
+    let heartCount = Number(getInnerText("heart-count"));
+    heartCount++;
+    document.getElementById("heart-count").innerText = heartCount;
+    console.log("clicked");
+  });
+}
+
+//* function calling
+addEventListenerToIncreaseHeart("heart1");
+addEventListenerToIncreaseHeart("heart2");
+addEventListenerToIncreaseHeart("heart3");
+addEventListenerToIncreaseHeart("heart4");
+addEventListenerToIncreaseHeart("heart5");
+addEventListenerToIncreaseHeart("heart6");
+addEventListenerToIncreaseHeart("heart7");
+addEventListenerToIncreaseHeart("heart8");
+addEventListenerToIncreaseHeart("heart9");
